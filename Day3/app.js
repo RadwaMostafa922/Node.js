06a5b3bf1628cd9ba0da14550b73e26b43d530ad
RadwaMostafa922 <radwamostafa922@gmail.com>
2022-04-14 09:15:36 +0200

    Day 3 ITI Course

--- Day3/app.js
+++ Day3/app.js
@@ -2,6 +2,7 @@ let login ,home ,profile ,signup;
 const http = require('http');
 const fs1 = require('fs').promises;
 const fs = require("fs");
+const qs = require('querystring');
 
 http.createServer(function(req, res) 
 {
@@ -14,56 +15,68 @@ http.createServer(function(req, res)
     }
     else if (req.url === "/login" && req.method === 'POST')
     {
-        //res.end(login);
-        var body = {};
+        var body={};
         req.on('data', chunk => {
-            body += JSON.parse(chunk.toString());
-            let rawdata = fs.readFileSync('./data.json');
-            let data = JSON.parse(rawdata);           
-            if((body.email === data.email) && (body.password === data.password))
+            body += chunk;
+            let reqData =qs.parse(body);
+            console.log(reqData)
+            //Read data from file:
+            const fileData = JSON.parse(fs.readFileSync("data.json").toString());
+            console.log(fileData);
+
+            if((reqData.email === undefined) || (reqData.password === undefined))
+            {
+                res.write('Please Enter your data');
+            }
+            else if((reqData.email === fileData.email) && (reqData.password === fileData.password))
             {
                 res.writeHead(200);
                 res.end(profile);
             }
-            else if((email !== data.email) || (password !== data.password))
+            else
             {
-                res.write('Please enter valid data');
+                res.writeHead(200);
+                res.write('Please Enter valid data');
             }
         });
     }
     else if(req.url === "/signup" && req.method === 'POST')
     {
-        const {email, password, confirm_password} = req.body
-        if(!isValidEmail(email) && (password !== confirm_password)){
-            res.write('Email must be Valide and Passwords must be same')
-        }
-        else if(!isValidEmail(email)){
-            res.write('Email must be Valide')
-        }
-        else if(password !== confirm_password)
-        {
-            res.write('Passwords must be same')
-        }
-        else
-        {
-            res.writeHead(200);
-            res.end(signup);
-            // var jsonObj = JSON.parse(jsonData);
-            // console.log(jsonObj);
-            
-            // // stringify JSON Object
-            // var jsonContent = JSON.stringify(jsonObj);
-            // console.log(jsonContent);
-            
-            // fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
-            //     if (err) {
-            //         console.log("An error occured while writing JSON Object to File.");
-            //         return console.log(err);
-            //     }
-            
-            //     console.log("JSON file has been saved.");
-            // });
-        }
+        var body={};
+        req.on('data', chunk => {
+            body += chunk;
+            let reqData =qs.parse(body);
+           //console.log(reqData)  //read requested data
+            //Read data from file:
+            const fileData = JSON.parse(fs.readFileSync("data.json").toString());
+            //console.log(fileData);
+
+            if((body.email === undefined) || (body.password === undefined) ||( body.confirm_password === undefined))
+            {
+                res.write('Please Enter your data');
+            }
+            else if(reqData.email === fileData.email)
+            {
+                res.write('Email already exists');
+            }
+            else if(reqData.password !== reqData.confirm_password)
+            {
+                res.write('Passwords must be same');
+            }
+            else
+            {
+                var email = reqData.email;
+                var password = reqData.password;
+                var data ={
+                    "email":email,
+                    "password":password
+                };
+                //console.log(email ,password);
+                fs.writeFileSync("./data.json", JSON.stringify(data));
+                res.writeHead(200);
+                res.end(profile);
+            }
+        });
     }
     else if (req.url ===  "/profile")
     {
